78809192486a31a8f48e45dc9297860082d57039
Athul-Kumar <athulkumar7991@gmail.com>
2023-04-20 09:33:25 +0530

    Day 5 list Basics (About file) is completed

--- Lists/AboutLIst.py
+++ Lists/AboutLIst.py
@@ -27,3 +27,33 @@ new_list = [item_1,item_2,item_3]
 help(new_list.methodName)
 
 """
+
+# lists with initial values.We use len() to find the length of  a list
+
+fruits = ['banana', 'orange', 'mango', 'lemon']
+veggies = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot']
+diary_products = ['milk', 'meat', 'butter', 'yoghurt']
+web_techs = ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'Postgres']
+countries = ['India', 'Nepal', 'Bhutan', 'China', 'Korea']
+
+# print lists and its length
+
+print('fruits: ', fruits)
+print('Number of fruits: ', len(fruits))
+print('Vegetables: ', veggies)
+print('Number of vegetables: ',len(veggies))
+print('Diary products: ',diary_products)
+print('Number of diary products: ', len(diary_products))
+print('Web technologies: ', web_techs)
+print('Number of Web technologies: ', len(web_techs))
+print('Countries: ',countries)
+print('Number of countries: ', len(countries))
+
+
+"""
+Also Lists can have items of different data types
+
+"""
+lst = ['Athul', 25, {'gender': 'Male'}, 60.50]
+print(type(lst))
+print(type(lst[2]))
