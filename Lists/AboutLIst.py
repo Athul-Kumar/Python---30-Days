61de5e9e75da2a277f7a65c5297505849633bf63
Athul-Kumar <athulkumar7991@gmail.com>
2023-04-20 09:20:56 +0530

    Day 5 about list file is added

---
+++ Lists/AboutLIst.py
@@ -0,0 +1,29 @@
+
+"""
+Lists --> is a collection which is ordered and changeable(modifiable, mutable).Allows duplicate members.
+        A list can be empty or it may have different data type items
+
+"""
+
+"""
+                    How to create a list
+                    
+method-1 --> using list build-in function
+
+lst = list() #this is an empty list
+
+method-2 --> using square brackets[]
+
+lst = []  #this is an empty list
+
+                    How to find methods associated with list using terminal
+                    
+new_list = [item_1,item_2,item_3]
+
+1.dir(new_list)
+
+                To know more about the method
+                
+help(new_list.methodName)
+
+"""
